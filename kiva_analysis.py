4ecb13d872a7cd4575c8f10bc69b67a8773fa1bf
Michael Cohen <michaelcohen@Michaels-Air.fios-router.home>
2021-02-22 18:28:43 -0500

    wrote code to pull subset of data from S3

--- kiva_analysis.py
+++ kiva_analysis.py
@@ -4,8 +4,16 @@
 import pandas as pd
 import numpy as np
 import datetime
+import boto3
 
-loan = pd.read_csv('./kiva_ds_csv/loans.csv', nrows=10000)
+
+s3 = boto3.client('s3')
+bucket = 'kiva-data'
+file_name = 'loans.csv'
+
+#reads in 10,000 row subset of dataframe
+obj = s3.get_object(Bucket= bucket, Key= file_name) 
+test_df = pd.read_csv(obj['Body'], nrows=10000) 
 
 loan.columns
 loan.info()
@@ -25,3 +33,9 @@ print(type(converted))
 #alternatively 
 jl['posted_date'] = pd.to_datetime(jl['POSTED_TIME'])
 
+loan['posted_datetime'] = pd.to_datetime(loan['POSTED_TIME'])
+
+#function to stop EC2 instance at end of script 
+def stop_EC2_instance(instance_id, region='us-west-2'):
+    ec2 = boto3.resource('ec2', region_name=region)
+    ec2.instances.filter(InstanceIds=[instance_id]).stop()
